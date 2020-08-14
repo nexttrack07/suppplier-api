5134695554879b702b2752e9d21f069a25aec6e6
Faadhil Hakim <faadhil1991@Faadhils-MBP.attlocal.net>
2020-08-14 16:54:01 -0500

    feat: configured admin and user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with email is successful"""
+        email = 'test@gmail.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_email_normalized(self):
+        """Test creating a user with email in uppercase"""
+        email = 'test@GM.com'
+        user = get_user_model().objects.create_user(email, 'test')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1124')
+
+    def test_create_new_super_user(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
