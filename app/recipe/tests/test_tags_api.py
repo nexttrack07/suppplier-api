b2a1709c2f87ca02820be9e9da286bd69ea0ada0
Faadhil Hakim <faadhil1991@Faadhils-MBP.attlocal.net>
2020-08-16 16:02:28 -0500

    feat: setup create tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -58,3 +58,20 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
 
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tags(self):
+        """Test creating a tag via post"""
+        payload = {'name': 'Zesty'}
+        res = self.client.post(TAGS_URL, payload)
+
+        tags = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(tags.data[0]['name'], 'Zesty')
+
+    def test_create_invalid_tag(self):
+        """Test creating a blank string tag"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
